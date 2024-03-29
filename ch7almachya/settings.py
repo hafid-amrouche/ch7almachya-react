37733594e839b2378971c4be0d2247ab278674c8
unknown <hafidamroucheg@gmail.com>
2024-03-29 06:24:05 +0100

    'm'

--- ch7almachya/settings.py
+++ ch7almachya/settings.py
@@ -14,11 +14,11 @@ from pathlib import Path
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import firebase_admin
-from firebase_admin import credentials
+# import firebase_admin
+# from firebase_admin import credentials
 
-cred = credentials.Certificate(BASE_DIR / 'ch7almachya/ch7al-machya-firebase-adminsdk-kjgyl-f37564c21f.json')
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate(BASE_DIR / 'ch7almachya/ch7al-machya-firebase-adminsdk-kjgyl-f37564c21f.json')
+# firebase_admin.initialize_app(cred)
 
 
 # Quick-start development settings - unsuitable for production
